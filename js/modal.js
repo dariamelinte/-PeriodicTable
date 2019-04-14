06486ab85a6ec3ba386fb194a4f4d30e9322f4bf
OEM Configuration (temporary user) <cruduiulik@gmail.com>
2019-04-14 19:33:49 +0300

    Modal made

---
+++ js/modal.js
@@ -0,0 +1,39 @@
+//Get the elements
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementsByClassName('close')[0];
+const openBtn = document.getElementById('openBtn');
+let elements = document.getElementsByClassName('hoverable');
+
+
+//Making the click events
+openBtn.addEventListener('click',openModal);
+closeBtn.addEventListener('click',closeModal);
+window.addEventListener('click',outsideClick);
+for(let elem of elements) {
+    elem.addEventListener('click',elementClick);
+}
+
+
+//Opens the element's modal
+function elementClick(e) {
+    let element = data[e.target.title]; // Gets the object with the e.target.title key
+    console.log(element);
+    openModal();
+}
+
+//Opens the modal
+function openModal() {
+    modal.style.display = "block";
+}
+
+//Closes the modal
+function closeModal() {
+    modal.style.display = "none";
+}
+
+//Closes the modal if clicked outside of it
+function outsideClick(e) {
+    if(e.target == modal) {
+        modal.style.display = "none";
+    }
+}
---
+++ resources/elements.js
@@ -0,0 +1,21 @@
+const data = {
+    "Hydrogen": {
+        "Nume": "Hidrogen",
+        "Simbol" : "H",
+        "Clasificare": "Nemetal",
+        "Grupa": "1 A",
+        "Perioada": 1,
+        "Bloc": "S",
+        "Numar atomic": 1,
+        "Masa atomica": "1.0079 uam",
+        "Configuratie electronica": "1S1",
+        "Numarul de protoni/electroni": 1,
+        "Numarul de neutroni": 0,
+        "Densitatea la 293 K": "0.08988 g/cm3",
+        "Punctul de topire": "-259.14 °C",
+        "Punctul de fierbere": "-252.87 °C",
+        "Stare de agregare": "Solida",
+        "Structura cristalina": "Hexagonala",
+        "Culoare" : "Incolor"
+    }
+}
