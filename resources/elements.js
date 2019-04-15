01d9541b0d86fd6f2d46c58da87fbaf52ec02c43
dariamelinte <dariamelinte2003@gmail.com>
2019-04-15 19:54:05 +0300

    'Elements and index'

--- resources/elements.js
+++ resources/elements.js
@@ -6,122 +6,523 @@ const data = {
         "Grupa": "1 A",
         "Perioada": 1,
         "Bloc": "S",
-        "Numar atomic": 1,
-        "Masa atomica": "1.0079 uam",
-        "Configuratie electronica": "1S1",
-        "Numarul de protoni/electroni": 1,
-        "Numarul de neutroni": 0,
+        "Număr atomic": 1,
+        "Masă atomică": "1.0079 uam",
+        "Configurație electronică": "1s1",
+        "Numărul de protoni/electroni": 1,
+        "Numărul de neutroni": 0,
         "Densitatea la 293 K": "0.08988 g/cm3",
         "Punctul de topire": "-259.14 °C",
         "Punctul de fierbere": "-252.87 °C",
-        "Stare de agregare": "Solida",
-        "Structura cristalina": "Hexagonala",
-        "Culoare" : "Incolor"
+        "Stare de agregare": "Solidă",
+        "Structura cristalină": "Hexagonală",
+        "Culoarea" : "Incolor"
     },
 
 
     "Helium" : {
-
+        "Nume" : "Heliu",
+        "Simbol" :	"He",
+        "Clasificare" :	"Gaz nobil",
+        "Grupa" : "I A",
+        "Perioada"	: "1",
+        "Bloc" : "S",
+        "Număr atomic" : "2",
+        "Masă atomică" : "4.002602 uam",
+        "Configurație electronică" : "1s2",
+        "Numarul de protoni/electroni" : "2",
+        "Numarul de neutroni" : "2",
+        "Densitatea la 293 K" : "0.1785 g/cm3",
+        "Punctul de topire" : "-272.0 °C",
+        "Punctul de fierbere" :	"-268.6 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Incolor"
     },
 
 
     "Lithium" : {
-
+        "Nume" : "Litiu",
+        "Simbol" : "Li",
+        "Clasificare" :	"Metal alcalin",
+        "Grupa" : "I A",
+        "Perioada" : "2",
+        "Bloc" : "S",
+        "Număr atomic" : "3",
+        "Masă atomică"	: "6.941 uam",
+        "Configurație electronică" : "1s2 2s1",
+        "Numărul de protoni/electroni" : "3",
+        "Numărul de neutroni" :	"4",
+        "Densitatea la 293 K" : "0.53 g/cm3",
+        "Punctul de topire" : "180.54 °C",
+        "Punctul de fierbere" :	"1347.0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie"
     },
 
 
     "Beryllium" : {
-
+        "Nume" : "Beriliu",
+        "Simbol" : "Be",
+        "Clasificare" :	"Alcalino-pământos",
+        "Grupa" : "II A",
+        "Perioada" : "2",
+        "Bloc" : "S",
+        "Număr atomic" : "4",
+        "Masă atomică"	: "9.012 uam",
+        "Configurație electronică" : "1s2 2s2",
+        "Numărul de protoni/electroni" : "4",
+        "Numărul de neutroni" :	"5",
+        "Densitatea la 293 K" : "1.847 g/cm3",
+        "Punctul de topire" : "1278.0 °C",
+        "Punctul de fierbere" :	"2970.0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Gri"
     },
 
 
     "Boron" : {
-
+        "Nume" : "Bor",
+        "Simbol" : "B",
+        "Clasificare" :	"Metaloid",
+        "Grupa" : "III A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "5",
+        "Masă atomică"	: "10.881 uam",
+        "Configurație electronică" : "	1s2 2s2 2p1",
+        "Numărul de protoni/electroni" : "5",
+        "Numărul de neutroni" :	"6",
+        "Densitatea la 293 K" : "2.34 g/cm3",
+        "Punctul de topire" : "2 075.9 °C",
+        "Punctul de fierbere" :	"3 926.9 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Romboedrică",
+        "Culoare" :	"Negricioasă"
     },
 
 
     "Carbon" : {
-
+        "Nume" : "Carbon",
+        "Simbol" : "C",
+        "Clasificare" :	"Nemetal",
+        "Grupa" : "IV A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "6",
+        "Masă atomică"	: "12.0107 uam",
+        "Configurație electronică" : "	1s2 2s2 2p2",
+        "Numărul de protoni/electroni" : "6",
+        "Numărul de neutroni" :	"6",
+        "Densitatea la 293 K" : "2.72 g/cm3",
+        "Punctul de topire" : " 3500 °C",
+        "Punctul de fierbere" :	"4 826.9 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Neagră"
     },
 
 
     "Nitrogen" : {
-
+        "Nume" : "Azot",
+        "Simbol" : "N",
+        "Clasificare" :	"Nemetal",
+        "Grupa" : "V A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "7",
+        "Masă atomică"	: "14.0067 uam",
+        "Configurație electronică" : "	1s2 2s2 2p3",
+        "Numărul de protoni/electroni" : "7",
+        "Numărul de neutroni" :	"7",
+        "Densitatea la 293 K" : "1.25 g/cm3",
+        "Punctul de topire" : "-210 °C",
+        "Punctul de fierbere" :	"-195,8 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Incolor"
     },
 
 
     "Oxygen" : {
-
+        "Nume" : "Oxigen",
+        "Simbol" : "O",
+        "Clasificare" :	"Nemetal",
+        "Grupa" : "VI A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "8",
+        "Masă atomică"	: "	15,9994 uam",
+        "Configurație electronică" : "1s2 2s2 2p4",
+        "Numărul de protoni/electroni" : "7",
+        "Numărul de neutroni" :	"8",
+        "Densitatea la 293 K" : "1.429 g/cm3",
+        "Punctul de topire" : "-218.4 °C",
+        "Punctul de fierbere" :	"-183.0 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Incolor"
     },
 
 
     "Fluorine" : {
-
+        "Nume" : "Fluor",
+        "Simbol" : "F",
+        "Clasificare" :	"Halogen",
+        "Grupa" : "VII A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "9",
+        "Masă atomică"	: "18,998 uam",
+        "Configurație electronică" : "	1s2 2s2 2p5",
+        "Numărul de protoni/electroni" : "9",
+        "Numărul de neutroni" :	"10",
+        "Densitatea la 293 K" : "1.696 g/cm3",
+        "Punctul de topire" : "-219.62 °C",
+        "Punctul de fierbere" :	"-188.14 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Verde"
     },
 
 
     "Neon" : {
-
+        "Nume" : "Neon",
+        "Simbol" : "Ne",
+        "Clasificare" :	"Gaz nobil",
+        "Grupa" : "VIII A",
+        "Perioada" : "2",
+        "Bloc" : "P",
+        "Număr atomic" : "10",
+        "Masă atomică"	: "20,17 uam",
+        "Configurație electronică" : "	1s2 2s2 2p6",
+        "Numărul de protoni/electroni" : "10",
+        "Numărul de neutroni" :	"10",
+        "Densitatea la 293 K" : "0.901 g/cm3",
+        "Punctul de topire" : "-248.6 °C",
+        "Punctul de fierbere" :	"-246.1 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Incolor"
     },
 
 
     "Sodium" : {
-
+        "Nume" : "Natriu / Sodiu",
+        "Simbol" : "Na",
+        "Clasificare" :	"Metal alcalin",
+        "Grupa" : "I A",
+        "Perioada" : "3",
+        "Bloc" : "S",
+        "Număr atomic" : "11",
+        "Masă atomică"	: "22,98 uam",
+        "Configurație electronică" : "	1s2 2s2 2p6 3s1",
+        "Numărul de protoni/electroni" : "11",
+        "Numărul de neutroni" :	"12",
+        "Densitatea la 293 K" : "0,971 g/cm3",
+        "Punctul de topire" : "97,72 °C",
+        "Punctul de fierbere" :	"883,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie"
     },
 
 
     "Magnesium" : {
-
+        "Nume" : "Magneziu",
+        "Simbol" : "Mg",
+        "Clasificare" :	"Metal alcalino-pământos",
+        "Grupa" : "II A",
+        "Perioada" : "3",
+        "Bloc" : "S",
+        "Număr atomic" : "12",
+        "Masă atomică"	: "24,305 uam",
+        "Configurație electronică" : "	1s2 2s2 2p6 3s2",
+        "Numărul de protoni/electroni" : "12",
+        "Numărul de neutroni" :	"12",
+        "Densitatea la 293 K" : "1,73 g/cm3",
+        "Punctul de topire" : "650,0 °C",
+        "Punctul de fierbere" :	"1090,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Alb, Gri metalic"
     },
 
 
     "Aluminium" : {
-
+        "Nume" : "Aluminiu",
+        "Simbol" : "Al",
+        "Clasificare" :	"Metal de post-tranziție",
+        "Grupa" : "III A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "13",
+        "Masă atomică"	: "	26,981 uam",
+        "Configurație electronică" : "	1s2 2s2 2p6 3s2 3p1",
+        "Numărul de protoni/electroni" : "13",
+        "Numărul de neutroni" :	"14",
+        "Densitatea la 293 K" : "2,702 g/cm3",
+        "Punctul de topire" : "660,32 °C",
+        "Punctul de fierbere" :	"2466,85 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie"
     },
 
 
     "Silicon" : {
-
+        "Nume" : "Siliciu",
+        "Simbol" : "Si",
+        "Clasificare" :	"Metaloid",
+        "Grupa" : "IV A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "14",
+        "Masă atomică"	: "	28,085 uam",
+        "Configurație electronică" : "	1s2 2s2 2p6 3s2 3p2",
+        "Numărul de protoni/electroni" : "14",
+        "Numărul de neutroni" :	"14",
+        "Densitatea la 293 K" : "2,33 g/cm3",
+        "Punctul de topire" : "1414,0 °C",
+        "Punctul de fierbere" :	"3265,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Gri închis"
     },
 
 
     "Phosphorus" : {
+        "Nume" : "Fosfor",
+        "Simbol" : "P",
+        "Clasificare" :	"Nemetal",
+        "Grupa" : "V A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "15",
+        "Masă atomică"	: "	30,973 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p3",
+        "Numărul de protoni/electroni" : "15",
+        "Numărul de neutroni" :	"16",
+        "Densitatea la 293 K" : "1,82 g/cm3",
+        "Punctul de topire" : "44,2 °C",
+        "Punctul de fierbere" :	"280,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Monoclinică",
+        "Culoare" :	"Alb, roșu și negru"
 
     },
 
 
-    "Sulfurus" : {
-
+    "Sulfur" : {
+        "Nume" : "Sulf",
+        "Simbol" : "S",
+        "Clasificare" :	"Nemetal",
+        "Grupa" : "VI A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "16",
+        "Masă atomică"	: "	32,065 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p4",
+        "Numărul de protoni/electroni" : "16",
+        "Numărul de neutroni" :	"16",
+        "Densitatea la 293 K" : "2,07 g/cm3",
+        "Punctul de topire" : "115,2 °C",
+        "Punctul de fierbere" :	"444,7 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Ortorombică",
+        "Culoare" :	"Galben"
     },
 
 
     "Chlorine" : {
-
+        "Nume" : "Clor",
+        "Simbol" : "Cl",
+        "Clasificare" :	"Halogen",
+        "Grupa" : "VII A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "17",
+        "Masă atomică"	: "	35,45 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p5",
+        "Numărul de protoni/electroni" : "17",
+        "Numărul de neutroni" :	"17",
+        "Densitatea la 293 K" : "3,21 g/cm3",
+        "Punctul de topire" : "-101,5 °C",
+        "Punctul de fierbere" :	"-34,0 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Ortorombică",
+        "Culoare" :	"Verde"
     },
 
 
-    "Argon" : {},
+    "Argon" : {   
+        "Nume" : "Argon",
+        "Simbol" : "Ar",
+        "Clasificare" :	"Gaz nobil",
+        "Grupa" : "VIII A",
+        "Perioada" : "3",
+        "Bloc" : "P",
+        "Număr atomic" : "18",
+        "Masă atomică"	: "	39,94 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6",
+        "Numărul de protoni/electroni" : "18",
+        "Numărul de neutroni" :	"22",
+        "Densitatea la 293 K" : "1,784 g/cm3",
+        "Punctul de topire" : "-189,3 °C",
+        "Punctul de fierbere" :	"-185,8 °C",
+        "Stare de agregare" : "Gazoasă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Incolor"
+    },
 
 
-    "Potassium" : {},
+    "Potassium" : {
+        "Nume" : "Potasiu",
+        "Simbol" : "K",
+        "Clasificare" :	"Metal alcalin",
+        "Grupa" : "I A",
+        "Perioada" : "4",
+        "Bloc" : "S",
+        "Număr atomic" : "19",
+        "Masă atomică"	: "	39,10 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 4s1",
+        "Numărul de protoni/electroni" : "19",
+        "Numărul de neutroni" :	"20",
+        "Densitatea la 293 K" : "1,82 g/cm3",
+        "Punctul de topire" : "63,4 °C",
+        "Punctul de fierbere" :	"765,6 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie"
+    },
 
 
-    "Calcium" : {},
+    "Calcium" : {
+        "Nume" : "Calciu",
+        "Simbol" : "Ca",
+        "Clasificare" :	"Metal alcalino-pământos",
+        "Grupa" : "II A",
+        "Perioada" : "4",
+        "Bloc" : "S",
+        "Număr atomic" : "20",
+        "Masă atomică"	: "40,07 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 4s2",
+        "Numărul de protoni/electroni" : "20",
+        "Numărul de neutroni" :	"20",
+        "Densitatea la 293 K" : "1,55 g/cm3",
+        "Punctul de topire" : "841,9 °C",
+        "Punctul de fierbere" :	"1483,9 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie și metalică"
+    },
 
 
-    "Scandium" : {},
+    "Scandium" : {
+        "Nume" : "Scandiu",
+        "Simbol" : "Sc",
+        "Clasificare" :	"Metal de tranziție",
+        "Grupa" : "III B",
+        "Perioada" : "4",
+        "Bloc" : "D",
+        "Număr atomic" : "21",
+        "Masă atomică"	: "44,96 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 4s2 3d1",
+        "Numărul de protoni/electroni" : "21",
+        "Numărul de neutroni" :	"24",
+        "Densitatea la 293 K" : "3,0 g/cm3",
+        "Punctul de topire" : "1540,9 °C",
+        "Punctul de fierbere" :	"2829,9 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Alb-argintiu"
+    },
 
 
-    "Titanium" : {},
+    "Titanium" : {
+        "Nume" : "Titan",
+        "Simbol" : "Ti",
+        "Clasificare" :	"Metal de tranziție",
+        "Grupa" : "IV B",
+        "Perioada" : "4",
+        "Bloc" : "D",
+        "Număr atomic" : "22",
+        "Masă atomică"	: "47,88 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 4s2 3d2",
+        "Numărul de protoni/electroni" : "22",
+        "Numărul de neutroni" :	"26",
+        "Densitatea la 293 K" : "4,54 g/cm3",
+        "Punctul de topire" : "1660,0 °C",
+        "Punctul de fierbere" :	"3287,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Hexagonală",
+        "Culoare" :	"Argintie"
+    },
 
 
-    "Vanadium" : {},
+    "Vanadium" : {
+        "Nume" : "Vanadiu",
+        "Simbol" : "V",
+        "Clasificare" :	"Metal de tranziție",
+        "Grupa" : "V B",
+        "Perioada" : "4",
+        "Bloc" : "D",
+        "Număr atomic" : "23",
+        "Masă atomică"	: "50,94 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 4s2 3d3",
+        "Numărul de protoni/electroni" : "23",
+        "Numărul de neutroni" :	"28",
+        "Densitatea la 293 K" : "6,1 g/cm3",
+        "Punctul de topire" : "1920,0 °C",
+        "Punctul de fierbere" :	"3400,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Argintie"
+    },
 
 
-    "Chromium" : {},
+    "Chromium" : {
+        "Nume" : "Crom",
+        "Simbol" : "Cr",
+        "Clasificare" :	"Metal de tranziție",
+        "Grupa" : "VI B",
+        "Perioada" : "4",
+        "Bloc" : "D",
+        "Număr atomic" : "24",
+        "Masă atomică"	: "52,00 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 3d5 4s1",
+        "Numărul de protoni/electroni" : "24",
+        "Numărul de neutroni" :	"28",
+        "Densitatea la 293 K" : "7,19 g/cm3",
+        "Punctul de topire" : "1857,0 °C",
+        "Punctul de fierbere" :	"2672,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Alb-cenușiu"
+    },
 
 
-    "Manganese" : {},
+    "Manganese" : {
+        "Nume" : "Mangan",
+        "Simbol" : "Mn",
+        "Clasificare" :	"Metal de tranziție",
+        "Grupa" : "VII B",
+        "Perioada" : "4",
+        "Bloc" : "D",
+        "Număr atomic" : "25",
+        "Masă atomică"	: "54,94 uam",
+        "Configurație electronică" : "1s2 2s2 2p6 3s2 3p6 3d5 4s2",
+        "Numărul de protoni/electroni" : "25",
+        "Numărul de neutroni" :	"30",
+        "Densitatea la 293 K" : "7,43 g/cm3",
+        "Punctul de topire" : "1245,0 °C",
+        "Punctul de fierbere" :	"1962,0 °C",
+        "Stare de agregare" : "Solidă",
+        "Structura cristalină" : "Cubică",
+        "Culoare" :	"Gri / Argintiu"
+    },
 
 
     "Iron" : {},
