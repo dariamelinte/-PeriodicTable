9e8fbb838ea3a80da8a619ffb53e6f88e5f9d4fb
dariamelinte <dariamelinte2003@gmail.com>
2019-04-17 11:39:14 +0300

    nav js

---
+++ js/nav.js
@@ -0,0 +1,37 @@
+
+
+var navNode = document.getElementById('nav-bar');
+
+navNode.innerHTML =
+`<ul class="mainmenu">
+<li><a href="index.html" class="button"> Acasă </a></li>
+<li><a href="table_page.html" class="button"> Tabelul periodic </a></li>
+
+
+
+<li><a href="#" class="button"> Chimie anorganică </a>
+
+    <ul class="submenu">
+
+        <li><a href="" class="content"> submenu 1  </a></li>
+        <li><a href="" class="content"> submenu 2  </a></li>
+        <li><a href="" class="content"> submenu 3  </a></li>
+        <li><a href="" class="content"> submenu 4  </a></li>
+
+    </ul>
+
+</li>
+
+
+<li><a href="#" class="button"> Chimie organică </a>
+
+    <ul class="submenu">
+
+        <li><a href="" class="content"> submenu 1  </a></li>
+        <li><a href="" class="content"> submenu 2  </a></li>
+        <li><a href="" class="content"> submenu 3  </a></li>
+        <li><a href="" class="content"> submenu 4  </a></li>
+
+    </ul>
+</li>
+</ul>`
