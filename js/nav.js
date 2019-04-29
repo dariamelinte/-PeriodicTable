a3c092a603db87fee3469f0e5b3cef59d3a27663
Vlad Romila <romilavlad2001@gmail.com>
2019-04-29 19:33:17 +0300

    AD

--- js/nav.js
+++ js/nav.js
@@ -25,7 +25,7 @@ navNode.innerHTML =
     <ul class="submenu">
 
         <li><a href="elementele_corpului_uman.html"> Elementele corpului uman</a></li>
-        <li><a href=""> Minerale si Vitamine  </a></li>
+        <li><a href="minerale_si_vitamine.html"> Minerale si Vitamine  </a></li>
         <li><a href=""> Proteine  </a></li>
         <li><a href=""> Lipide  </a></li>
         <li><a href=""> Glucide  </a></li>
