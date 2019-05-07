979f48139268e573a03fa0b3ece4939f77dc2400
dariamelinte <dariamelinte2003@gmail.com>
2019-05-07 17:21:03 +0300

    daria

--- js/test.js
+++ js/test.js
@@ -2,16 +2,6 @@ var answers = document.getElementsByClassName("varianta");
 var questions = document.getElementsByClassName("question");
 var forms = document.getElementsByClassName("forms");
 var inputs = document.getElementsByClassName("input");
-
-// inputs.map(key => {
-//     console.log(inputs[key].value);
-//     console.log(inputs[key].checked);
-//     console.log(' ');
-// })
-
-// answers.map(function(answer) {
-//     console.log(answer);
-// });
 document.getElementById('button').addEventListener('click', () => {
     let k = 0, checked = 0;
     for (let input in inputs) {
@@ -19,11 +9,19 @@ document.getElementById('button').addEventListener('click', () => {
             checked++;
             if (inputs[input].value == 1) {
                 k++;
+
             }
         }
     }
     if (checked === 3)
-        document.getElementById("result").innerHTML = `Ati raspuns la ${k} intrebari din ${inputs.length / 3} corect!`
-    else
-        alert("Nu ai raspuns la toate intrebarile!");
+    {
+        if( k === 1 )
+            document.getElementById("result").innerHTML = `Ați răspuns la o întrebare corectă !`;
+        else
+            document.getElementById("result").innerHTML = `Ati raspuns la ${k} intrebari din ${inputs.length / 3} corect!`
+        else
+            alert("Nu ai raspuns la toate intrebarile!");
+
+    }
+        
 })
