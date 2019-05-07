9757e570dcfe224f168a9ea656a8d48450d4e606
Vlad Romila <romilavlad2001@gmail.com>
2019-05-07 13:05:28 +0300

    am adaugat JS pentru ca iulian nu se pricepe :D

--- js/test.js
+++ js/test.js
@@ -1,13 +1,29 @@
 var answers = document.getElementsByClassName("varianta");
 var questions = document.getElementsByClassName("question");
 var forms = document.getElementsByClassName("forms");
+var inputs = document.getElementsByClassName("input");
 
-console.log(answers);
+// inputs.map(key => {
+//     console.log(inputs[key].value);
+//     console.log(inputs[key].checked);
+//     console.log(' ');
+// })
 
 // answers.map(function(answer) {
 //     console.log(answer);
 // });
-
-for(var answer in answers) {
-    console.log(answers[answer]);
-}
+document.getElementById('button').addEventListener('click', () => {
+    let k = 0, checked = 0;
+    for (let input in inputs) {
+        if (inputs[input].checked === true) {
+            checked++;
+            if (inputs[input].value == 1) {
+                k++;
+            }
+        }
+    }
+    if (checked === 3)
+        document.getElementById("result").innerHTML = `Ati raspuns la ${k} intrebari din ${inputs.length / 3} corect!`
+    else
+        alert("Nu ai raspuns la toate intrebarile!");
+})
