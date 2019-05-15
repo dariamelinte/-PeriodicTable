03c9d6084c6e1f498037c83943df557eb76b380c
Iulian Crudu <iuliancrudu@d-soft.ro>
2019-05-15 22:29:28 +0300

    awa

---
+++ js/analize.js
@@ -0,0 +1,96 @@
+let btn = document.getElementsByClassName('submitBtn')[0];
+let inputs = document.getElementsByClassName('input-edit');
+let divInputs = document.getElementsByClassName('input-div');
+let tableRows = document.getElementsByClassName('table-row');
+let genders = document.getElementsByClassName('gender');
+let form = document.getElementsByClassName('form')[0];
+let mins = document.getElementsByClassName('min');
+let maxs = document.getElementsByClassName('max');
+
+form.addEventListener('change', (event) => {
+   let gender = event.target.value;
+   console.log(gender);
+   for(let input in inputs) {
+        let name = inputs[input].name;
+        let analiza = data[name];
+        if(!analiza)
+            continue;
+        if(analiza.matters === 'yes') {
+            mins[input].innerHTML = name === "male" ? analiza.minText.male : analiza.minText.female;
+            maxs[input].innerHTML = name === "male" ? analiza.maxText.male : analiza.maxText.female;
+        }
+   }
+});
+
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    // console.log(inputs);
+    for (let input in inputs) {
+        //Checks if the field is given
+        if (inputs[input].value) {
+
+            //Gets the gender
+            let gender = '';
+            if (genders[0].checked) {
+                gender = 'male';
+            } else {
+                gender = 'female';
+            }
+
+
+            //Getting all the fields and data needed
+            let name = inputs[input].name;
+            let analiza = data[name];
+            let value = inputs[input].value;
+            let divInput = divInputs[input];
+            let tableRow = tableRows[input];
+
+            if (analiza.matters === "no") {
+                let max = analiza.maxValue;
+                let min = analiza.minValue;
+                if (value >= min && value <= max) {
+                    //true
+                    inputs[input].style.display = "none";
+                    divInput.innerHTML += `<p>${value}</p>`;
+                    tableRow.classList.add("valid");
+
+                    console.log(true);
+                } else {
+                    //false
+                    console.log(false);
+                    inputs[input].style.display = "none";
+                    divInput.innerHTML += `<p>${value}</p>`;
+                    tableRow.classList.add("invalid");
+                }
+            } else {
+                let maxFemale = analiza.maxValue.female;
+                let minFemale = analiza.minValue.female;
+
+                let maxMale = analiza.maxValue.male;
+                let minMale = analiza.minValue.male;
+
+                if(gender === 'male') {
+                    if(value >=minMale && value <= maxMale) {
+                        inputs[input].style.display = "none";
+                        divInput.innerHTML += `<p>${value}</p>`;
+                        tableRow.classList.add("valid");
+                    } else {
+                        inputs[input].style.display = "none";
+                        divInput.innerHTML += `<p>${value}</p>`;
+                        tableRow.classList.add("invalid");
+                    }
+                } else {
+                    if(value >= minFemale && value <= maxFemale) {
+                        inputs[input].style.display = "none";
+                        divInput.innerHTML += `<p>${value}</p>`;
+                        tableRow.classList.add("valid");
+                    } else {
+                        inputs[input].style.display = "none";
+                        divInput.innerHTML += `<p>${value}</p>`;
+                        tableRow.classList.add("invalid");
+                    }
+                }
+            }
+        }
+    }
+});
---
+++ resources/analize.js
@@ -0,0 +1,42 @@
+const data = {
+    "Calciu Ionic Seric" : {
+        "matters": "no",
+        "minText" : "3.6 mg/dl",
+        "minValue": "3.6",
+        "maxText": "5.2 mg/dl",
+        "maxValue": "5.2"
+    },
+    "Magneziu Seric": {
+        "matters": "no",
+        "minText": "1.6 mg/dl",
+        "minValue": "1.6",
+        "maxText": "2.55 mg/dl",
+        "maxValue": "2.55",
+    },
+    "Potasiu Seric": {
+        "matters": "no",
+        "minText": "3.5 mEq/L",
+        "minValue": "3.5",
+        "maxText": "4.5 mEq/L",
+        "maxValue": "4.5",
+    },
+    "Sideremia": {
+        "matters": "yes",
+        "minText": {
+          "male": "100 % ml plasma",
+          "female": "90 % ml plasma"
+        },
+        "minValue": {
+          "male": "100",
+          "female": "90",
+        },
+        "maxText": {
+            "male": "160 % ml plasma",
+            "female": "130 % ml plasma",
+        },
+        "maxValue": {
+            "male": "160",
+            "female": "130",
+        }
+    }
+};
