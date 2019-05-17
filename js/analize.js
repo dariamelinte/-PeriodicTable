a3879677b1e09bba84a60fb50cc76360450e9c29
Iulian Crudu <iuliancrudu@d-soft.ro>
2019-05-17 12:38:03 +0300

    wqs

--- js/analize.js
+++ js/analize.js
@@ -1,4 +1,4 @@
-let btn = document.getElementById("button");
+let btn = document.getElementsByClassName("submit")[0];
 let inputs = document.getElementsByClassName('input-edit');
 let divInputs = document.getElementsByClassName('input-div');
 let tableRows = document.getElementsByClassName('table-row');
@@ -8,103 +8,135 @@ let mins = document.getElementsByClassName('min');
 let maxs = document.getElementsByClassName('max');
 
 form.addEventListener('change', (event) => {
-   let gender = event.target.value;
-   console.log(gender);
-   for(let input in inputs) {
-        let name = inputs[input].name;
-        let analiza = data[name];
-        if(!analiza)
-            continue;
-        if(analiza.matters === 'yes') {
-            mins[input].innerHTML = name === "male" ? analiza.minText.male : analiza.minText.female;
-            maxs[input].innerHTML = name === "male" ? analiza.maxText.male : analiza.maxText.female;
-        }
-   }
+    let gender = event.target.value;
+    console.log(gender);
+    // for (let input in inputs) {
+    //     let name = inputs[input].name;
+    //     let analiza = data[name];
+    //     if (!analiza)
+    //         continue;
+    //     if (analiza.matters === 'yes') {
+    //         mins[input].innerHTML = name === "male" ? analiza.minText.male : analiza.minText.female;
+    //         maxs[input].innerHTML = name === "male" ? analiza.maxText.male : analiza.maxText.female;
+    //     }
+    // }
 });
 
 btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    // console.log(inputs);
-    for (let input in inputs) {
-        //Checks if the field is given
-        if (inputs[input].value) {
-
-            //Gets the gender
-            let gender = '';
-            if (genders[0].checked) {
-                gender = 'male';
-            } else {
-                gender = 'female';
-            }
+    if (btn.classList.contains('again')) {
+        //Reloads the whole page
+        location.reload();
+    } else {
 
 
-            //Getting all the fields and data needed
-            let name = inputs[input].name;
-            let analiza = data[name];
-            let value = inputs[input].value;
-            let divInput = divInputs[input];
-            let tableRow = tableRows[input];
+        event.preventDefault();
+        // console.log(inputs);
+        for (let input in inputs) {
+            //Checks if the field is given
+            if (inputs[input].value) {
 
-            if (analiza.matters === "no") {
-                let max = analiza.maxValue;
-                let min = analiza.minValue;
-                if (value >= min && value <= max) {
-                    //true
-                    inputs[input].style.display = "none";
-                    divInput.innerHTML += `<p>${value}</p>`;
-
-                    // tableRow.classList.add("valid");
-
-                    tableRow.className += " valid";
-
-                    console.log(true);
+                //Gets the gender
+                let gender = '';
+                if (genders[0].checked) {
+                    gender = 'male';
                 } else {
-                    //false
-                    console.log(false);
-                    inputs[input].style.display = "none";
-                    divInput.innerHTML += `<p>${value}</p>`;
-
-                    // tableRow.classList.add("invalid");
-                    
-                    tableRow.className += " invalid";
+                    gender = 'female';
                 }
+                //Getting all the fields and data needed
+                let name = inputs[input].name;
+                let analiza = data[name];
+                let value = inputs[input].value;
+                let divInput = divInputs[input];
+                let tableRow = tableRows[input];
 
-                divInput.className += " align";
-                maxs[input].className += " visible";
-                mins[input].className += " visible";
-                divInput.className +=" visible";
-
-            } else {
-                let maxFemale = analiza.maxValue.female;
-                let minFemale = analiza.minValue.female;
-
-                let maxMale = analiza.maxValue.male;
-                let minMale = analiza.minValue.male;
-
-                if(gender === 'male') {
-                    if(value >=minMale && value <= maxMale) {
+                if (analiza.matters === "no") {
+                    let max = analiza.maxValue;
+                    let min = analiza.minValue;
+                    if (value >= min && value <= max) {
+                        //true
                         inputs[input].style.display = "none";
                         divInput.innerHTML += `<p>${value}</p>`;
-                        tableRow.classList.add("valid");
+
+                        // tableRow.classList.add("valid");
+
+                        tableRow.className += " valid";
+
+                        console.log(true);
                     } else {
+                        //false
+                        console.log(false);
                         inputs[input].style.display = "none";
                         divInput.innerHTML += `<p>${value}</p>`;
-                        tableRow.classList.add("invalid");
+
+                        // tableRow.classList.add("invalid");
+
+                        tableRow.className += " invalid";
                     }
+
+                    //Shows mins and maxs
+                    divInput.className += " align";
+                    maxs[input].textContent = analiza.maxText;
+                    mins[input].textContent = analiza.minText;
+                    maxs[input].className += " visible";
+                    mins[input].className += " visible";
+                    divInput.className += " visible";
+
+
                 } else {
-                    if(value >= minFemale && value <= maxFemale) {
-                        inputs[input].style.display = "none";
-                        divInput.innerHTML += `<p>${value}</p>`;
-                        tableRow.classList.add("valid");
+                    let maxFemale = analiza.maxValue.female;
+                    let minFemale = analiza.minValue.female;
+
+                    let maxMale = analiza.maxValue.male;
+                    let minMale = analiza.minValue.male;
+
+                    if (gender === 'male') {
+                        if (value >= minMale && value <= maxMale) {
+                            inputs[input].style.display = "none";
+                            divInput.innerHTML += `<p>${value}</p>`;
+                            tableRow.classList.add("valid");
+                        } else {
+                            inputs[input].style.display = "none";
+                            divInput.innerHTML += `<p>${value}</p>`;
+                            tableRow.classList.add("invalid");
+                        }
+
+                        //Adds mins and maxs
+
+                        divInput.className += " align";
+                        maxs[input].textContent = analiza.maxText.male;
+                        mins[input].textContent = analiza.minText.male;
+                        maxs[input].className += " visible";
+                        mins[input].className += " visible";
+                        divInput.className += " visible";
+
                     } else {
-                        inputs[input].style.display = "none";
-                        divInput.innerHTML += `<p>${value}</p>`;
-                        tableRow.classList.add("invalid");
+                        if (value >= minFemale && value <= maxFemale) {
+                            inputs[input].style.display = "none";
+                            divInput.innerHTML += `<p>${value}</p>`;
+                            tableRow.classList.add("valid");
+                        } else {
+                            inputs[input].style.display = "none";
+                            divInput.innerHTML += `<p>${value}</p>`;
+                            tableRow.classList.add("invalid");
+                        }
+
+                        divInput.className += " align";
+                        maxs[input].textContent = analiza.maxText.female;
+                        mins[input].textContent = analiza.minText.female;
+                        maxs[input].className += " visible";
+                        mins[input].className += " visible";
+                        divInput.className += " visible";
                     }
                 }
+            } else {
+                tableRows[input].className += " hidden";
             }
-        } else {
-            tableRows[input].className += " hidden";
         }
+
+        btn.innerHTML = "Din Nou";
+        btn.className += " again";
+        btn.classList.remove("submit");
     }
 });
+
+
--- resources/analize.js
+++ resources/analize.js
@@ -38,5 +38,76 @@ const data = {
             "male": "160",
             "female": "130",
         }
+    },
+    "Sodiu Seric": {
+        "matters": "no",
+        "minText": "310 mg/100 mL",
+        "minValue": "310",
+        "maxText": "345 mg/100 mL",
+        "maxValue": "345",
+    },
+    "Clor Seric": {
+        "matters": "no",
+        "minText": "345 mg/100 mL",
+        "minValue": "345",
+        "maxText": "395 mg/100 mL",
+        "maxValue": "395",
+    },
+    "Fosforemia": {
+        "matters": "no",
+        "minText": "3.5 mg % ml sange",
+        "minValue": "3.5",
+        "maxText": "4.5 mg % ml sange",
+        "maxValue": "4.5",
+    },
+    "Glicemia": {
+        "matters": "no",
+        "minText": "70 mg/100 ml sange",
+        "minValue": "70",
+        "maxText": "110 mg/100 ml sange",
+        "maxValue": "110",
+    },
+    "Trigliceridele": {
+        "matters": "no",
+        "minText": "50 mg/100 ml",
+        "minValue": "50",
+        "maxText": "150 mg/100 ml",
+        "maxValue": "150",
+    },
+    "HDL": {
+        "matters": "no",
+        "minText": "35 mg/dl",
+        "minValue": "35",
+        "maxText": "100 mg/dl",
+        "maxValue": "100",
+    },
+    "LDL": {
+        "matters": "no",
+        "minText": "50 mg/dl",
+        "minValue": "50",
+        "maxText": "150 mg/dl",
+        "maxValue": "150"
+    },
+    "Lipidele Totale": {
+        "matters": "no",
+        "minText": "500 ml % sange",
+        "minValue": "500",
+        "maxText": "800 ml % sange",
+        "maxValue": "800"
+    },
+    "Acid Uric Seric Sange": {
+        "matters": "no",
+        "minText": "2 mg/100 ml ser",
+        "minValue": "2",
+        "maxText": "5 mg/100 ml ser",
+        "maxValue": "5"
+    },
+    "Acid Uric Seric Urina": {
+        "matters": "no",
+        "minText": "0.25 g/urina",
+        "minValue": "0.25",
+        "maxText": "0.8 g/urine",
+        "maxValue": "0.8"
     }
+
 };
